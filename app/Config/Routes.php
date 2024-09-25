b2d556d7f3da91d3a75b846ce53d7ebaacbfbac0
micorobles <130731528+micorobles@users.noreply.github.com>
2024-09-25 16:53:28 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,7 @@ use App\Controllers\Pages;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index'); // Root URL ( localhost:8080 )
 // $routes->get('/', 'pages::index');
-$routes->get('pages', [Pages::class, 'index']);
-$routes->get('(:segment)', [Pages::class, 'view']);
+$routes->get('pages', [Pages::class, 'index']); // Static URL ( localhost:8080/page )
+$routes->get('(:segment)', [Pages::class, 'view']); // Dynamic URL ( localhost:8080/page1 or /page2 or /page3 )
