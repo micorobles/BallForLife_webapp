8f197d9a854d246e5c7889c606af9e6274e9c0e2
micorobles <130731528+micorobles@users.noreply.github.com>
2024-10-17 20:58:49 +0800

    update
    profile template added

--- app/Views/Profile/profile.php
+++ app/Views/Profile/profile.php
@@ -12,7 +12,18 @@
 
 <?= $this->section('content'); ?>
 
-    <span>Profile Body</span>
+    <div class="container p-0">
+        <div class="cover-photo rounded">
+            <img src="<?= base_url('images/cover_photo.jpg') ?>" alt="">
+        </div>
+
+        <div class="profile-picture">
+            <img src="<?= base_url(session()->get('profilePic')) ?>" alt="">
+            <div class="profile-buttons">
+                <button class="btn btn-custom-color text-white font-sm light-text">Edit Profile <i class="fa-solid fa-user-pen fa-1x"></i></button>
+            </div>
+        </div>
+    </div>
 
 <?= $this->endSection(); ?>
 
