dde190032bb2b31e0b695d3d02b31d78c21d57e9
micorobles <130731528+micorobles@users.noreply.github.com>
2024-10-24 17:05:41 +0800

    update
    added datatable initialization

--- app/Views/Masters/usersMaster.php
+++ app/Views/Masters/usersMaster.php
@@ -4,7 +4,8 @@
 
 <?= $this->section('css'); ?>
 
-<?= load_css('Masters/userMaster.css') ?>
+<?= load_css('global/datatables.min.css') ?>
+<?= load_css('Masters/usersMaster.css') ?>
 
 <?= $this->endSection(); ?>
 
@@ -12,6 +13,38 @@
 
 <?= $this->section('content'); ?>
 
+<div class="container-fluid">
+    <table id="tblUser" >
+        <thead>
+            <tr>
+                <!-- <th hidden>ID</th> -->
+                <th>E-mail Address</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Position</th>
+                <th>Update Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <!-- <td hidden>1</td> -->
+                <td>mic@gmail.com</td>
+                <td>Mico</td>
+                <td>Robles</td>
+                <td>Member</td>
+                <td>10-24-24</td>
+            </tr>
+            <tr>
+                <!-- <td hidden>2</td> -->
+                <td>lyka@gmail.com</td>
+                <td>Lyka</td>
+                <td>Jiao</td>
+                <td>Point Guard</td>
+                <td>10-24-24</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 
 <?= $this->endSection(); ?>
@@ -20,7 +53,7 @@
 
 <?= $this->section('js'); ?>
 
-<?= load_js('global/global-functions.js'); ?>
+<?= load_js('global/datatables.min.js') ?>
 <?= load_js('Masters/userMaster.js'); ?>
 
 <?= $this->endSection(); ?>
---
+++ public/js/Masters/userMaster.js
@@ -0,0 +1,47 @@
+"use strict";
+(function () {
+    const UserMaster = function () {
+        return new UserMaster.init();
+    }
+    UserMaster.init = function () {
+        this.$tblUser = "";
+        this.tblctr = 0;
+    }
+    UserMaster.prototype = {
+        drawDataTables: function () {
+            var self = this;
+
+            if (!$('#tblUser').hasClass('dataTable')) {
+
+                $('#tblUser thead tr').clone(true).appendTo('#tblUser thead');
+                $('#tblUser thead tr:nth-child(2) th').each(function (i) {
+                    var title = $(this).text();
+                    $(this).html("<input type='text' class='form-control form-control-sm' placeholder='" + title + "' />");
+                });
+
+                // $('input', this).on('keyup change', function () {
+                //     if (self.$tblUser.column(i).search() !== this.value) {
+                //         self.$tblUser
+                //             .column(i)
+                //             .search(this.value)
+                //             .draw();
+                //     }
+                // });
+            }
+
+            self.$tblUser = $('#tblUser').DataTable({
+                orderCellsTop: true, // Keeps the sorting on the first row, not the second
+            });
+
+            return this;
+
+        },
+    }
+
+    UserMaster.init.prototype = UserMaster.prototype;
+
+    $(document).ready(function () {
+        var _U = UserMaster();
+        _U.drawDataTables();
+    });
+})();
