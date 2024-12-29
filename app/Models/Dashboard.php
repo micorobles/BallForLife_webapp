b48920b43ad908629e3a19ad9241d08d9d6eebd3
micorobles <130731528+micorobles@users.noreply.github.com>
2024-12-29 14:54:26 +0800

    update

--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -29,11 +29,28 @@ class Dashboard extends Model
     }
     public function getAppointmentRequests()
     {
-        return $this->db->table('schedules-appointment')->where('is_deleted', false)
-                                                                   ->where('status', 'Pending') 
-                                                                   ->countAllResults();
-    }
+        // return $this->db->table('schedules-appointment')->where('is_deleted', false)
+        //                                                            ->where('status', 'Pending') 
+        //                                                            ->countAllResults();
 
+        //     return $this->db->table('schedules-appointment')->select('COUNT(*) AS requests_count')
+        //                                                                ->join('schedules', 'schedID = schedules.ID', 'left') 
+        //                                                                ->where('schedules.startDate >', date('Y-m-d H:i:s'))
+        //                                                                ->where('schedules-appointment.is_deleted', false)
+        //                                                                ->where('status', 'Pending') 
+        //                                                                ->groupBy('schedules.startDate') // Ensure grouping to make COUNT work correctly
+        //                                                                ->countAllResults();
+        // }
+        $query = $this->db->table('schedules-appointment')
+                  ->join('schedules', 'schedID = schedules.ID', 'left')
+                  ->where('schedules.startDate >', date('Y-m-d H:i:s'))
+                  ->where('schedules-appointment.is_deleted', false)
+                  ->where('status', 'Pending')
+                  ->groupBy('schedules.startDate')
+                  ->get();
+
+        return count($query->getResultArray());
+    }
     // USERS
     
     public function getAppointmentPending()
